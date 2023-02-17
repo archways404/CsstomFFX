e1dfb598fe5d565b89d4302ad86eadcce336d112
archways <107893165+archways404@users.noreply.github.com>
2023-02-17 10:54:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import os
+#import customtkinter
 
-# Python program showing
-# a use of input()
+#customtkinter.set_appearance_mode("dark")
+#customtkinter.set_default_color_theme("dark-blue")
+
+#root = customtkinter.CTk()
+#root.geometry("1000x600")
+
+#def login():
+#    print("test")
+
+#frame = customtkinter.CTkFrame(master=root)
+#frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+#label = customtkinter.CTkLabel(master=frame, text="Enter the full path to your profile folder: ")
+#label.pack(pady=12, padx=10)
+
+#entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Path goes here")
+#entry1.pack(pady=12, padx=10)
+
+#button_path = customtkinter.CTkButton(master=frame, text="Continue", command=login)
+#button_path.pack(pady=12, padx=10)
+
+#root.mainloop()
+
+# GRAPHICAL SETUP - IGNORE FOR NOW
  
 path_to_userprofile_folder = input("Enter the full path to your profile folder: ")
 print(path_to_userprofile_folder)
@@ -17,8 +40,19 @@ if not os.path.exists(path_to_userprofile_folder + "/chrome"):
 rw_path = path_to_userprofile_folder + "/chrome"
 
 # Check if the right files exist within the folder (rw_path) - i.e pictures etc.
+#if it does not exist, download them from github
 
 # Reading the file
+rw_path_css = rw_path + "/userChrome.css"
+
+if os.path.exists(rw_path_css):
+    print("warning: userChrome.css already exists within the chrome folder - continuing will overwrite it")
+else:
+    with open(rw_path_css, "w") as fp:
+        fp.write("")
+
+
+
 
 # Library of settings
 #   Matching true or false on each setting & displaying in in the GUI as such
